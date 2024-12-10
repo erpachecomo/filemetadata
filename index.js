23b7da624430face8b4756478ef1278d1f93c985
erpachecomo <epachecomorelos@gmail.com>
2024-12-09 16:51:27 -0800

    add file handling

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-
-
-
+//  * 2. You can submit a form that includes a file upload.
+//  * 3. The form file input field has the name attribute set to upfile.
+//  * 4. When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  return res.json({name: originalname, type: mimetype, size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
